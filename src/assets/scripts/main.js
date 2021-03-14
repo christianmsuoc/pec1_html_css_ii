7f0ce16324400b8173b94356bf24eb787295182a
christianms <munhiz.christian@gmail.com>
2021-03-14 11:38:19 +0100

    Avances en maquetación

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,4 +1,18 @@
 const menuButton = document.getElementById('navbar-menu-button');
 const mobileNav = document.getElementById('mobile-nav');
 
-menuButton.addEventListener('click', () => mobileNav.classList.toggle('no-display'));
+/* Toggle nav */
+menuButton.addEventListener(
+  'click',
+  (e) => {
+    e.stopPropagation()
+    mobileNav.classList.toggle('no-display')
+  });
+
+/* Prevent menu form close when click inside */
+mobileNav.addEventListener('click', (e) => e.stopPropagation());
+
+/* Hide nav if clicked outside */
+window.addEventListener('click', () => {
+  mobileNav.classList.add('no-display');
+});
