afe20ee44126225cf79196204efb9cfcaee2f206
Christian <munhiz.christian@gmail.com>
2021-03-17 21:12:34 +0100

    Finalización de maquetación móvil

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,18 +1,27 @@
-const menuButton = document.getElementById('navbar-menu-button');
-const mobileNav = document.getElementById('mobile-nav');
+const jquery = require("jquery");
+window.$ = window.jQuery = jquery;
+require("jquery/dist/jquery.min.js");
 
-/* Toggle nav */
-menuButton.addEventListener(
-  'click',
-  (e) => {
+$(function() {
+  const mobileNav = $('#mobile-nav');
+
+  /* Toggle nav */
+  $('#navbar-menu-button').on('click', (e) => {
     e.stopPropagation()
-    mobileNav.classList.toggle('no-display')
+    mobileNav.toggleClass('no-display')
   });
 
-/* Prevent menu form close when click inside */
-mobileNav.addEventListener('click', (e) => e.stopPropagation());
+  /* Prevent menu form close when click inside */
+  mobileNav.on('click', (e) => e.stopPropagation());
 
-/* Hide nav if clicked outside */
-window.addEventListener('click', () => {
-  mobileNav.classList.add('no-display');
+  /* Hide nav if clicked outside */
+  window.addEventListener('click', () => {
+    mobileNav.addClass('no-display');
+  });
+
+  $('a').on('click', function(event) {
+    mobileNav.addClass('no-display');
+  });
 });
+
+
